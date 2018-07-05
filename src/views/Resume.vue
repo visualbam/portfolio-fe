7d2dbc38a8509788395efb28cfcd6e37220d596c
Bruce McElroy <bruce.mcelroy@gmail.com>
2018-07-04 21:48:40 -0400

    make resume data driven

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -7,75 +7,40 @@
             <v-divider></v-divider>
             <v-container>
                 <v-layout style="align-items: center;">
-                    <v-flex pa-4 xs3>
+                    <v-flex pa-4 s12 md3>
                         <img src="@/assets/ptc-full-logo.svg" alt="">
                     </v-flex>
-                    <v-flex pa-4 xs3>
+                    <v-flex pa-4 s12 md3>
                         <img src="@/assets/ppg-logo.svg" alt="">
                     </v-flex>
-                    <v-flex pa-4 xs3>
+                    <v-flex pa-4 s12 md3>
                         <img src="@/assets/bb-logo.svg" alt="">
                     </v-flex>
-                    <v-flex pa-4 xs3>
+                    <v-flex pa-4 s12 md3>
                         <img src="@/assets/acatar-logo.svg" alt="">
                     </v-flex>
                 </v-layout>
             </v-container>
         </v-card>
         <v-card class="ma-5 pa-4">
-            <h3>Experience</h3>
+            <h3> <v-icon>work</v-icon>  Work Experience</h3>
+            <br>
             <v-list three-line>
-                <template>
+                <template v-for="company in companies">
                     <v-subheader>
-                        Software Engineer
+                        {{company.position}}
                     </v-subheader>
 
                     <v-divider></v-divider>
 
                     <v-list-tile>
                         <v-list-tile-avatar>
-                            <img src="@/assets/acatar-icon-logo.svg" alt="">
+                            <img :src="company.logo" alt="">
                         </v-list-tile-avatar>
 
                         <v-list-tile-content>
-                            <v-list-tile-title>Feb 2016 - Nov 2017</v-list-tile-title>
-                            <v-list-tile-sub-title>Acatar</v-list-tile-sub-title>
-                        </v-list-tile-content>
-                    </v-list-tile>
-                </template>
-                <template>
-                    <v-subheader>
-                        Software Engineer
-                    </v-subheader>
-
-                    <v-divider></v-divider>
-
-                    <v-list-tile>
-                        <v-list-tile-avatar>
-                            <img src="@/assets/bb-icon-logo.svg" alt="">
-                        </v-list-tile-avatar>
-
-                        <v-list-tile-content>
-                            <v-list-tile-title>Feb 2016 - Nov 2017</v-list-tile-title>
-                            <v-list-tile-sub-title>Branding Brand</v-list-tile-sub-title>
-                        </v-list-tile-content>
-                    </v-list-tile>
-                </template>
-                <template>
-                    <v-subheader>
-                        Software Engineer
-                    </v-subheader>
-
-                    <v-divider></v-divider>
-
-                    <v-list-tile>
-                        <v-list-tile-avatar>
-                            <img src="@/assets/ppg-icon-logo.svg" alt="">
-                        </v-list-tile-avatar>
-
-                        <v-list-tile-content>
-                            <v-list-tile-title>Feb 2016 - Nov 2017</v-list-tile-title>
-                            <v-list-tile-sub-title>PPG Architectural Coatings</v-list-tile-sub-title>
+                            <v-list-tile-title>{{company.startDate}} - {{company.endDate}}</v-list-tile-title>
+                            <v-list-tile-sub-title>{{company.name}}</v-list-tile-sub-title>
                         </v-list-tile-content>
                     </v-list-tile>
                 </template>
@@ -83,6 +48,67 @@
         </v-card>
     </div>
 </template>
+
+<script>
+    export default {
+        name: 'Resume',
+        data() {
+            return {
+                companies: [
+                    {
+                        logo: '',
+                        name: 'FI360',
+                        position: 'Software Engineer',
+                        startDate: 'Jan 2018',
+                        endDate: 'Present'
+                    },
+                    {
+                        logo: '',
+                        name: 'Berryville Holdings, LLC',
+                        position: 'Software Engineer',
+                        startDate: 'February 2016',
+                        endDate: 'November 2017'
+                    },
+                    {
+                        logo: '@/assets/acatar-icon-logo.svg',
+                        name: 'Acatar',
+                        position: 'Software Engineer',
+                        startDate: 'January 2015',
+                        endDate: 'January 2016'
+                    },
+                    {
+                        logo: '@/assets/acatar-icon-logo.svg',
+                        name: 'Branding Brand',
+                        position: 'Frontend Web Developer',
+                        startDate: 'November 2013',
+                        endDate: 'August 2014'
+                    },
+                    {
+                        logo: '@/assets/acatar-icon-logo.svg',
+                        name: 'PPG Architectural Coatings',
+                        position: 'Frontend Web Developer',
+                        startDate: 'August 2012',
+                        endDate: 'November 2013'
+                    },
+                    {
+                        logo: '@/assets/acatar-icon-logo.svg',
+                        name: 'Kolano Design',
+                        position: 'Graphic Designer / Frontend Web Developer',
+                        startDate: 'August 2011',
+                        endDate: 'November 2013'
+                    },
+                    {
+                        logo: '@/assets/acatar-icon-logo.svg',
+                        name: 'Pittsburgh Technical College',
+                        position: 'Visual Communications Assistant',
+                        startDate: 'August 2012',
+                        endDate: 'November 2013'
+                    }
+                ]
+            }
+        }
+    }
+</script>
 
 <style scoped>
 
